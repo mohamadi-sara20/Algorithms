a741c8483385e93b8cc36d281734c2a86b60ced2
sara <mohamadi.sara20@gmail.com>
2019-09-27 21:56:25 +0330

    added my root method

--- src/main/java/algorithms/UF.java
+++ src/main/java/algorithms/UF.java
@@ -2,11 +2,21 @@ package algorithms;
 public class UF {
     private int[] id;
 
-    public UF(int N){this.id = new int[N];
-    for(int i=0; i < N; i++){
-        id[i] = i;
-        }
+    public static void main(String[] args){
+        int[] a = {0, 1, 2, 3, 4, 1, 5, 1, 7, 8};
+        int b = 8;
+        UF uf = new UF(10);
+        uf.id = a;
+        System.out.println(uf.root(8));
+        System.out.println(uf.root(9));
+        System.out.println(uf.root(5));
 
+
+    }
+
+    public UF(int N){
+        this.id = new int[N];
+        for(int i=0; i < N; i++) id[i] = i;
     }
 
     public boolean connected(int p, int q){
@@ -17,27 +27,15 @@ public class UF {
         int pid = id[p];
         int qid = id[q];
         for(int i = 0; i < this.id.length; i++){
-            if (id[i] == pid){
+            if (id[i] == pid)
                 id[i] = qid;
-            }
         }
     }
 
     private int root(int p){
-        while (p != id[p]){
-            p = id[p];
-        }
-        return p;
+        if(p == id[p]) return p;
+        else return root(id[p]);
     }
 
-    public boolean quickFind(int p, int q){return root(p) == root(q);}
 
-    public void quickUnion(int p, int q){
-        int pid = root(p);
-        int qid = root(q);
-        id[pid] = qid;
-    }
-    public void weightedQuickUnion(int p, int q){
-
-    }
 }
