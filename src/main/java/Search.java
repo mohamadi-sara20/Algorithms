b369cb32947edcb459029d60af878e23dc521680
sara <mohamadi.sara20@gmail.com>
2020-04-27 15:51:46 +0430

    recursive binary search added

--- src/main/java/Search.java
+++ src/main/java/Search.java
@@ -23,13 +23,24 @@ public class Search {
 		}
 	}
 
+	public int recursiveBinarySearch(int num, int start, int end){
+		int mid = (start + end) / 2;
+		if(id[mid] == num){return mid;}
+		else if(num > id[mid]){return recursiveBinarySearch(num, mid + 1, end);}
+		else if(num < id[mid]){return recursiveBinarySearch(num, start, mid - 1);}
+		return -1;
+	}
+
+
 
 	
 
 	public static void main(String[] args){
-		int[] d = {};
+		int[] d = {10, 20, 30, 40, 50, 60, 70};
 		Search search = new Search(d);
-		System.out.println(search.binarySearch(3));
+		System.out.println(search.recursiveBinarySearch(70, 0, d.length));
+
+
 		
 	}
 
