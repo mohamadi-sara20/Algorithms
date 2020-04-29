3de22dd8e9a76ea98a82dfea606edbe7e669e740
sara <mohamadi.sara20@gmail.com>
2020-04-29 15:09:30 +0430

    a minor bug fixed

--- src/main/java/ArrayQueue.java
+++ src/main/java/ArrayQueue.java
@@ -8,7 +8,7 @@ public class ArrayQueue{
 	}
 
 	public void enqueue(String i){		
-		if(N == s.length && N > 0) resize(s.length * 2);
+		if(N == s.length  && N > 0) resize(s.length * 2);
 		s[N++] = i;
 	}
 
@@ -16,11 +16,12 @@ public class ArrayQueue{
 		String i = s[B];
 		s[B] = null;
 		B++;
+		//when queue is emptied but indices are reset. 
+		//when N is 1; e.g., and B is 0; and you perform dequeue, B becomes 1 like N. But they should both be reset. 
+		if(B == N) {B = 0; N = 0;}
 
-		if(N - B == (s.length / 4) && N-B > 0){
+		if(N - B == (s.length / 4) && N - B > 0){
 			resize(s.length / 2);
-			B = 0;
-			N = 0;
 
 		}
 		return i;
@@ -30,12 +31,17 @@ public class ArrayQueue{
 	private void resize(int capacity){
 		System.out.print("Resizing to: " );
 		System.out.println(capacity);
+
+
 		String[] new_s = new String[capacity];
 		int j = 0;
 		for(int i = B; i < N; i++){
 			new_s[j] = s[i];
 			j++;
 		}
+
+		N = N - B;
+		B = 0;
 		s = new_s;
 	}
 
@@ -52,20 +58,20 @@ public class ArrayQueue{
 		a.enqueue("2");
 		a.enqueue("3");
 		a.enqueue("4");
-		a.enqueue("8");
-		a.enqueue("8");
-		a.enqueue("8");
-		a.enqueue("8");
-		a.display_queue();
-		a.dequeue();
-		a.dequeue();
-		a.dequeue();
-		a.dequeue();
-		a.enqueue("4");
-		a.dequeue();
 		a.enqueue("5");
 		a.enqueue("6");
+		a.enqueue("7");
+		a.enqueue("8");
 		a.dequeue();
 		a.dequeue();
+		a.dequeue();
+		a.dequeue();
+		a.dequeue();
+		a.dequeue();
+		a.dequeue();
+		a.dequeue();
+		a.enqueue("8");
+		a.display_queue();
+
 	}
 }
