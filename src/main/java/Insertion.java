cbfd65d244e9071c816b69a320a32f535e9ab597
sara <mohamadi.sara20@gmail.com>
2020-04-30 15:05:58 +0430

    compressed two variables into 1;

--- src/main/java/Insertion.java
+++ src/main/java/Insertion.java
@@ -2,18 +2,17 @@ public class Insertion{
 
 	public int[] sort(int[] a){
 		for(int i = 1; i < a.length; i++){
-			int j = i - 1;
-			int current_ind = i;
-			while(j >= 0 && a[j] > a[current_ind]){
-				int temp = a[current_ind];
-				a[current_ind] = a[j];
+			int j = i;
+			//compare part
+			while(j >= 1 && a[j-1] > a[j]){
+				int temp = a[j-1];
+				a[j-1] = a[j];
 				a[j] = temp;
-				current_ind = current_ind - 1;
 				j = j - 1;
 			}
 		}
 	return a;
-}
+	}
 
 	public void display(int[] arr){
 		for(int i = 0; i < arr.length; i++) System.out.print(arr[i]);
@@ -23,8 +22,11 @@ public class Insertion{
 	public static void main(String[] args){
 		Insertion i = new Insertion();
 		int[] a = {5, 4, 3, 1};
+		int[] b = {5, 9, 4, 1, 8, 4, 2};
 		// i.sort(a);
 		i.display(i.sort(a));
+		i.display(i.sort(b));
+
 	}
 
 
