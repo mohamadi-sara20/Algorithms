4a592f66d54dcf358c95a4446657a60c8809f1d5
sara <mohamadi.sara20@gmail.com>
2020-04-30 21:36:00 +0430

    shell bugs fixed

--- src/main/java/Shell.java
+++ src/main/java/Shell.java
@@ -1,45 +1,36 @@
 public class Shell{
-	private int[] h;
-	public Shell(int[] h){
-		this.h = h;
-	}
+	private int[] gap_arr = {1, 3, 5, 7, 16};
 
 	public int[] sort(int[] a){
-		int[] h_arr = {3, 2, 1};
-		for(int c= 0; c < this.h.length; c++){
-		int h = this.h[c];
-		int i = 0;
-		while(i < a.length - h){		
-		int a1 = a[i];
-		int a2 = a[i + h];
-		if(a1 > a2){
-			a[i] = a2;
-			a[i + h] = a1;
+		for(int gap: gap_arr){
+			for(int i = gap; i < a.length; i++){
+				for(int j= i; j >= gap && a[j - gap] > a[j]; j=j-gap){
+					int temp = a[j];
+					a[j] = a[j-gap];
+					a[j-gap] = temp;
+				}
 			}
-		i++;
 		}
-	}
 		return a;
-	}
-
-	public void display(int[] arr){
-		for(int i = 0; i < arr.length; i++) System.out.print(arr[i]);
-		System.out.println();
-	}
-
-	public void setH(int[] h){this.h = h;}
-	public static void main(String[] args){
-		int[] h1 = {3, 2, 1};
-		Shell shell = new Shell(h1);
-		int[] a = {5, 9, 4, 1, 8, 3, 2};
-		shell.display(shell.sort(a));
-
-		int[] h2 = {5, 3, 1};
-		shell.setH(h2);
-		int[] b = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
-		shell.display(shell.sort(b));
 
 	}
+
+
+
+	public void display(int[]a){
+	        for(int i=0;i<a.length;i++)System.out.println(a[i]);
+	    }
+
+
+
+    public static void main(String[] args) {
+        int[] a = {10,9,8,7,6,5,4,3,2,1};
+        Shell shell = new Shell();
+        shell.display(shell.sort(a));
+
+        
+    }
+
 
 
 }
